fae39e6b2b4b56f6581f0be24719817c71a932fd
Diana C. Rojas R <dianacarolinarojas@gmail.com>
2026-01-22 18:40:53 +0100

    feat: Actualizar version de python
    Actualizar version de python a 3.8 e instalar tini para enviar signals desde el host al contenedor.

--- 12.0/entrypoint.sh
+++ 12.0/entrypoint.sh
@@ -16,17 +16,20 @@ echo -e "\n-------- ODOO_CONF $ODOO_CONF --------"
 : ${PASSWORD:=${DB_PASSWORD:=${POSTGRES_PASSWORD:='odoo'}}}
 : ${DATABASE:=${DB_NAME:='odoo'}}
 
+# Initialize DB_ARGS as a proper array
+DB_ARGS=("--config=${ODOO_CONF}")
 
-DB_ARGS=(" --config=${ODOO_CONF}")
 function check_config() {
     param="$1"
     value="$2"
     # if the given parameter is not found in the ODOO_CONF file,
-    # add it to the DB_ARGS variable with the given value.
+    # add it to the DB_ARGS array with the given value.
     if ! grep -q -E "^\s*\b${param}\b\s*=" "$ODOO_CONF" ; then
-        DB_ARGS+=" --${param} ${value}"
-    fi;
+        DB_ARGS+=("--${param}")
+        DB_ARGS+=("${value}")
+    fi
 }
+
 # pass postgres parameters as arguments to the odoo process 
 # if not present in the config file
 check_config "db_host" "$HOST"
@@ -40,7 +43,7 @@ function check_odoo_repo() {
         echo -e "\n-------- cloning odoo repository --------"
         cd "$ODOO_SRC"
         git clone https://github.com/odoo/odoo.git --depth=1 -b $ODOO_VERSION
-   fi;
+   fi
 }
 
 check_odoo_repo
@@ -97,16 +100,14 @@ case "$1" in
     -- | odoo)
         shift
         if [[ "$1" == "scaffold" ]] ; then
-            python3 $ODOO_EXEC $@
+            exec python3 "$ODOO_EXEC" "$@"
         else
-            python3 $ODOO_EXEC $@ ${DB_ARGS[@]}
+            exec python3 "$ODOO_EXEC" "$@" "${DB_ARGS[@]}"
         fi
         ;;
     -*)
-        python3 $ODOO_EXEC $@ ${DB_ARGS[@]}
+        exec python3 "$ODOO_EXEC" "$@" "${DB_ARGS[@]}"
         ;;
     *)
         exec "$@"
 esac
-
-exit 1
